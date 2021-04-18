c293e02ea506863d69feb089639443ed97437bde
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-04-18 22:32:44 +0530

    stock application started

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import streamlit as st
-st.write('Hello, world!')
-x = st.slider('x')
-st.write(x, 'squared is', x * x)
+import yfinance as yf
+import pandas as pd
+
+st.write("""
+# Stock Price Streamlit Application
+
+Apple's stock and volume over the time
+
+""")
+
+stockSymbol = 'AAPL'
+stockData = yf.Ticker(stockSymbol)
+stockDf = stockData.history(period='1d', start='2011-10-10', end='2021-01-31')
+st.line_chart(stockDf.Close)
+st.line_chart(stockDf.Volume)
+
+#x = st.slider('x')
+#st.write(x, 'squared is', x * x)
