8bf314d47559bb33631d4dff9a7bd49acafbcc73
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2024-04-13 22:25:11 +0000

    Revert "fix datetime issue"
    This reverts commit 3c55acdf6b1ff982c275187e8f213dbcbfc5a9f9.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import datetime
 import streamlit as st
-from datetime import datetime
-import pytz
 from get_all_tickers import get_tickers as gt
+# from get_all_tickers.get_tickers import Region
 import yfinance as yf
 import pandas as pd
-
 st.set_page_config(page_title='Market Summary App', page_icon="📈", layout='centered', initial_sidebar_state='auto')
 
 hide_streamlit_style = """
@@ -25,12 +23,15 @@ pd.set_option("display.max_rows", None, "display.max_columns", None)
 df = pd.read_csv('symbols.csv')
 df1 = df[['symbol']]
 result = df1.head(None)
+# result.to_csv('tickers.csv') 
+# print(result)
 
 stock = st.selectbox(
 'Which stock you would like to select?',
 (result))
 
 stock_name = df.loc[df['symbol'] == stock, 'name'].iloc[0]
+# name = stock_name.head(None)
 st.write('You have selected:', stock_name)
 
 col1, col2 = st.columns(2)
@@ -39,12 +40,11 @@ with col1:
 with col2:
     To = st.date_input("To", datetime.date.today(), min_value=None, max_value=None, key=None)
 
-# Convert From and To dates to datetime objects with UTC timezone
-From = datetime.combine(From, datetime.min.time()).replace(tzinfo=pytz.UTC)
-To = datetime.combine(To, datetime.min.time()).replace(tzinfo=pytz.UTC)
-
 stockSymbol = stock
 stockData = yf.Ticker(stockSymbol)
 stockDf = stockData.history(period='1d', start=From, end=To)
 st.line_chart(stockDf.Close)
 st.line_chart(stockDf.Volume)
+
+#x = st.slider('x')
+#st.write(x, 'squared is', x * x)
