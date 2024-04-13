e2b2709c0a0c40f17ce28c4f909b2c6bd15c36d8
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2024-04-13 22:29:32 +0000

    fix datetime issue

--- main.py
+++ main.py
@@ -40,9 +40,13 @@ with col1:
 with col2:
     To = st.date_input("To", datetime.date.today(), min_value=None, max_value=None, key=None)
 
+# Convert From and To dates to UTC timestamps
+from_timestamp = datetime.datetime.combine(From, datetime.datetime.min.time()).timestamp()
+to_timestamp = datetime.datetime.combine(To, datetime.datetime.min.time()).timestamp()
+
 stockSymbol = stock
 stockData = yf.Ticker(stockSymbol)
-stockDf = stockData.history(period='1d', start=From, end=To)
+stockDf = stockData.history(period='1d', start=from_timestamp, end=to_timestamp)
 st.line_chart(stockDf.Close)
 st.line_chart(stockDf.Volume)
 
